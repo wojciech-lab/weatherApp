edb65f818d66d4d0f4fa141b7cb036ba81dde8e0
Wojciech-Lab <Kantorwojciech2002@gmail.com>
2026-01-16 20:39:29 +0100

    second commit

---
+++ services/Openeather_api.py
@@ -0,0 +1,28 @@
+from config import Config
+import requests
+from datetime import datetime
+
+URL= f"https://api.openweathermap.org/data/2.5/weather?q={Config.OPENWEATHER_CITY}&appid={Config.OPENWEATHER_API_KEY}"
+
+def fetch_weather():
+    try:
+        response = requests.get(URL)
+        data = response.json()
+
+        weather_dict = {
+            "miasto": data.get("name"),
+            "temperatura": data.get("main", {}).get("temp"),
+            "odczuwalna_temperatura": data.get("main", {}).get("feels_like"),
+            "wilgotność": data.get("main", {}).get("humidity"),
+            "ciśnienie": data.get("main", {}).get("pressure"),
+            "wiatr": data.get("wind", {}).get("speed"),
+            "data_pomiaru": datetime.now().strftime("%Y-%m-%d")
+        }
+
+        return weather_dict
+
+    except Exception as e:
+        print(e)
+
+
+
--- weather_app.py
+++ weather_app.py
@@ -1,5 +1,17 @@
 from config import Config
+from services.Openeather_api import fetch_weather
+import time
 
-print(Config.OPENWEATHER_API_KEY)
-print(Config.OPENWEATHER_CITY)
 
+while True:
+    weather = fetch_weather()
+    print("test")
+    if weather:
+        print(f"Data pomiaru: {weather['data_pomiaru']}")
+        print(f"Miasto: {weather['miasto']}")
+        print(f"Temperatura: {weather['temperatura']} °C")
+        print(f"Odczuwalna: {weather['odczuwalna_temperatura']} °C")
+        print(f"Wilgotność: {weather['wilgotność']} %")
+        print(f"Ciśnienie: {weather['ciśnienie']} hPa")
+        print(f"Wiatr: {weather['wiatr']} m/s")
+        time.sleep(60)
